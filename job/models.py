3869a20eb43ba54e4b7fdb0d20d6a30ff7c35935
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2020-04-18 18:14:03 +0100

    Add Resource model

--- job/models.py
+++ job/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Job(models.Model):
     pid = models.AutoField(primary_key=True)  # Primary ID
@@ -12,4 +12,20 @@ class Job(models.Model):
     description = models.CharField(max_length=250, default='')
     url = models.CharField(max_length=250, default='')
     visible = models.BooleanField(default=False)
+
+class Resource(models.Model):
+    CATEGORIES = (
+        ('OPEN', 'OPEN'),
+        ('ACCEPTED', 'ACCEPTED'),
+        ('DECLINED', 'DECLINED'),
+    )
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=250, default='')
+    description = models.TextField(default='')
+    url = models.CharField(max_length=250, default='')
+    category = models.CharField(max_length=250, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='resource')
+
 
