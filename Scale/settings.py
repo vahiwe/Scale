192972793df390782faf98bd2619c43ba56b6db5
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2020-04-18 19:32:01 +0100

    Update settings

--- Scale/settings.py
+++ Scale/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
 CELERY_BROKER_URL = 'amqp://localhost'
 
 LOGOUT_REDIRECT_URL = 'index'
